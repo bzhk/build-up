5009ff123a2047fc8719ec1da7e68bd472312d2d
bzhk <bzhk911@gmail.com>
2017-09-23 01:19:08 +0200

    render Tasks name

--- comps/DetailsComps/Rows/ProceduresDetails.js
+++ comps/DetailsComps/Rows/ProceduresDetails.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, ListView } from 'react-native';
 import PropTypes from 'prop-types';
 
-
+const styles = StyleSheet.create({
+  listView:{
+    marginTop: 20,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginBottom: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#F7F7F7',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '600',
+  },
+});
 export default class ProceduresDetails extends React.Component {
   constructor(props) {
     super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(props.navigation.state.params.rowData.tasks),
+    };
+    console.log(props.navigation.state.params.rowData.tasks)
+    console.log(this.state.dataSource)
   }
 
   static navigationOptions = ({ navigation }) => ({
@@ -14,11 +39,16 @@ export default class ProceduresDetails extends React.Component {
   render(){
 
     return (
-      <View>
-        <Text>
-            asdf
-        </Text>
-      </View>
+      <ListView
+        style={styles.listView}
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) =>
+          <View style={styles.container}>
+            <Text style={styles.text}>{rowData.name}</Text>
+          </View>
+
+        }
+        />
     );
   }
 }
